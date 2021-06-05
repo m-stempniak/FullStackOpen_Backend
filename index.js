c1ece5647562a6854ec4628132ea9839c5c86024
Mariusz Stempniak <m.stempniak@zoho.com>
2021-06-05 04:46:11 +0200

    update

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
